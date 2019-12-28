d05ba9811cd91fba726da4278af9d0b8036eb5da
Eric Haskins <ehask71@gmail.com>
2019-12-28 02:24:10 -0500

    fixes

--- lib/blocs/login/login_form.dart
+++ lib/blocs/login/login_form.dart
@@ -1,8 +1,16 @@
+import 'package:blaze_chat/user_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:blaze_chat/blocs/login/login.dart';
 
 class LoginForm extends StatefulWidget {
+  final UserRepository _userRepository;
+
+  LoginForm({Key key, @required UserRepository userRepository})
+      : assert(userRepository != null),
+        _userRepository = userRepository,
+        super(key: key);
+
   @override
   State<LoginForm> createState() => _LoginFormState();
 }
@@ -10,6 +18,22 @@ class LoginForm extends StatefulWidget {
 class _LoginFormState extends State<LoginForm> {
   final _usernameController = TextEditingController();
   final _passwordController = TextEditingController();
+  LoginBloc _loginBloc;
+
+  bool get isPopulated =>
+      _usernameController.text.isNotEmpty && _passwordController.text.isNotEmpty;
+
+  bool isLoginButtonEnabled(LoginState state) {
+    return state.isFormValid && isPopulated && !state.isSubmitting;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loginBloc = BlocProvider.of<LoginBloc>(context);
+    _usernameController.addListener(_onUsernameChanged);
+    _passwordController.addListener(_onPasswordChanged);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -47,11 +71,22 @@ class _LoginFormState extends State<LoginForm> {
                   TextFormField(
                     decoration: InputDecoration(labelText: 'Email'),
                     controller: _usernameController,
+                    keyboardType: TextInputType.emailAddress,
+                    autovalidate: true,
+                    autocorrect: false,
+                    validator: (_) {
+                      return !state.isEmailValid ? 'Invalid Email' : null;
+                    },
                   ),
                   TextFormField(
                     decoration: InputDecoration(labelText: 'Password'),
                     controller: _passwordController,
                     obscureText: true,
+                    autovalidate: true,
+                    autocorrect: false,
+                    validator: (_) {
+                      return !state.isPasswordValid ? 'Invalid Password' : null;
+                    },
                   ),
                   ConstrainedBox(
                       constraints:
@@ -75,4 +110,31 @@ class _LoginFormState extends State<LoginForm> {
       ),
     );
   }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  void _onUsernameChanged() {
+    _loginBloc.add(
+      UsernameChanged(email: _usernameController.text),
+    );
+  }
+
+  void _onPasswordChanged() {
+    _loginBloc.add(
+      PasswordChanged(password: _passwordController.text),
+    );
+  }
+
+  void _onFormSubmitted() {
+    _loginBloc.add(
+      LoginButtonPressed(
+        username: _usernameController.text,
+        password: _passwordController.text,
+      ),
+    );
 }
