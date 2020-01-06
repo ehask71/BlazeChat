4cd91b662984cddefafcf276917d3f208883571a
Eric Haskins <ehask71@gmail.com>
2020-01-06 02:20:02 -0500

    Adding Home Page Bloc,Event, and State

--- lib/group_repository.dart
+++ lib/group_repository.dart
@@ -1,17 +1,19 @@
 import 'dart:async';
 import 'package:http/http.dart';
 import 'package:http_interceptor/http_interceptor.dart';
+import 'package:blaze_chat/common/api_interceptor.dart';
 import 'package:blaze_chat/blocs/groups/groups.dart';
+import 'package:blaze_chat/user_repository.dart';
 
 class GroupRepository {
-  HttpClientWithInterceptor client;
+  HttpClientWithInterceptor client = HttpClientWithInterceptor.build(interceptors: [ApiInterceptor()]);
 
   GroupRepository(this.client);
 
   Future<List> _fetchGroups(int startIndex, int limit) async {
 
     final token = await _userRepository.getToken();
-    final response = await _httpClient.get(
+    final response = await client.get(
         Constants().API +'groups?_start=$startIndex&_limit=$limit');
   }
 }
